6b6287e85ba034fdeac288b24e45cd7f4c4a599b
Alon Bassok <bassoka@wsdot.wa.gov>
2019-11-19 12:56:21 -0800

    remove unneeded sites from locations

--- r1_address_geocoding.R
+++ r1_address_geocoding.R
@@ -51,5 +51,27 @@ ggmap::register_google(key = api, account_type = "standard")
 locations_df <- locations %>%
   ggmap::mutate_geocode(full_address)
 
+# Remove large Urban or small Rural grantees that are not subject to the site visit requirements for WSDOT
+# per email from Seth Stark and Steven Meyeroff on 20191119
+locations_df <- locations_df %>%
+  dplyr::filter(!(Grantee == "Ben Franklin Transit" |
+                    Grantee == "City of Selah" |
+                    Grantee == "City of Yakima" |
+                    Grantee == "Clark County Public Transportation Benefit Area (C - Tran)" |
+                    Grantee == "Community Action of Skagit County" |
+                    Grantee == "Entiat Valley Community Services (EVCS)" |
+                    Grantee == "Everett Transit" |
+                    Grantee == "King County Metro Transit" |
+                    Grantee == "Kitsap County Public Transportation Benefit Area Authority" |
+                    Grantee == "Lower Elwha Klallam Tribe" |
+                    Grantee == "Pierce Transit" |
+                    Grantee == "Samish Indian Nation" |
+                    Grantee == "Snohomish County Workforce Development Council" |
+                    Grantee == "Spokane Transit Authority" |
+                    Grantee == "Stanwood Community & Senior Center" |
+                    Grantee == "Thurston County Public Benefit Transportation Area (Intercity Transit)" |
+                    Grantee == "Whatcom Transportation Authority" |
+                    Grantee == "Yakima Valley Conference of Governments (YVCOG)"))
+
 # Export list of addresses for giggles
 write.csv(locations_df, "data/locations_df.csv", row.names = FALSE)

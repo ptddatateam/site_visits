f55300d39b2f37e13f70dddcde09904c82cd64e2
Alon Bassok <bassoka@wsdot.wa.gov>
2019-11-19 17:09:49 -0800

    working on popup for types and dates since visit

--- r4_shiny_app.R
+++ r4_shiny_app.R
@@ -3,6 +3,7 @@
 
 # packages
 library(readr) # sane way of reading files
+library(openxlsx) # open xlsx files, note that readxl doesn't read urls... Come on Hadley...
 library(tidyverse) # for all that is good and holy in this world
 library(leaflet) # javascript web mapping
 library(leaflet.extras) # full screen control among other useful things
@@ -11,8 +12,47 @@ library(shiny)
 # Set Working directory to Github folder, change path for your machine
 # setwd("~/GitHub/site_visits")
 
-# Bring in the clean data frame
+# Bring in the clean data frame with distances
 grantee_df <- readr::read_csv("data/grantee_df.csv")
+
+# Bring in the data on visit type and date since last visit
+types <- openxlsx::read.xlsx("http://sharedot/eso/so/pubtcb/Docs/2019-11-08 - S_V Criteria Prioritization Draft.xlsx",
+                             startRow = 5,
+                             cols = 2:6,
+                             detectDates = TRUE) %>%
+  dplyr::rename(grantee = Grantee,
+                admin = Date.of.Last.Site.Visit,
+                capital = X3,
+                drug = X4, 
+                financial = X5) %>%
+  dplyr::filter(!(grantee == "Ben Franklin Transit" |
+                    grantee == "City of Selah" |
+                    grantee == "City of Yakima" |
+                    grantee == "Clark County Public Transportation Benefit Area (C - Tran)" |
+                    grantee == "Community Action of Skagit County" |
+                    grantee == "Entiat Valley Community Services (EVCS)" |
+                    grantee == "Everett Transit" |
+                    grantee == "King County Metro Transit" |
+                    grantee == "Kitsap County Public Transportation Benefit Area Authority" |
+                    grantee == "Lower Elwha Klallam Tribe" |
+                    grantee == "Pierce Transit" |
+                    grantee == "Samish Indian Nation" |
+                    grantee == "Snohomish County Workforce Development Council" |
+                    grantee == "Spokane Transit Authority" |
+                    grantee == "Stanwood Community & Senior Center" |
+                    grantee == "Thurston County Public Benefit Transportation Area (Intercity Transit)" |
+                    grantee == "Whatcom Transportation Authority" |
+                    grantee == "Yakima Valley Conference of Governments (YVCOG)")) %>%
+  tidyr::pivot_longer(c("admin","capital","drug","financial"),
+                      names_to = "type",
+                      values_to = "date_since") %>%
+  dplyr::filter(substr(date_since,1,1)=="2") %>%
+  dplyr::mutate(date_since = as.Date(date_since)) %>%
+  dplyr::mutate(days_since = Sys.Date() - date_since) %>%
+  dplyr::select(-date_since) %>%
+  tidyr::pivot_wider(names_from = type,
+                     values_from = days_since) %>%
+  dplyr::select(grantee, admin, capital, drug, financial)
 
 # Make the shiny app
 shinyApp(
@@ -31,6 +71,8 @@ shinyApp(
   
   server = function(input, output) {
     
+    types2 <- reactive({types})
+    
     filtered <- reactive({
       grantee_df[grantee_df$grantee_origin == input$siteName &
                    grantee_df$minutes < 60,]
@@ -42,6 +84,10 @@ shinyApp(
     
     flitered_single <- reactive({
       unique(filtered_single[,c(1:3)],)
+    })
+    
+    flitered_single <- reactive({
+      dplyr::inner_join(filtered_single, types2, by = c("grantee_origin" = "grantee"))
     })
     
     output$siteSelect <- renderUI({
@@ -90,7 +136,12 @@ shinyApp(
                            lat = fdata_single$lat_origin,
                            radius = 20,
                            color = "#E69F00",
-                           popup = as.character(fdata_single$grantee_origin)) %>%
+                           popup = paste(fdata_single$grantee_origin, "<br>",
+                                         "Admin", fdata_single$admin,"<br>",
+                                         "Capital", fdata_single$capital,"<br>",
+                                         "Drug & alcohol", fdata_single$drug,"<br>",
+                                         "Financial", fdata_single$financial)) %>%
+                           #popup = as.character(fdata_single$grantee_origin)) %>%
           fitBounds(minLong,minLat,maxLong,maxLat)
       }else{
         MapPlot1_proxy %>%
